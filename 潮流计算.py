98c3a4f0f9fe0beef82fe8cf9c62fe91f920ad20
Ekai <yvyikai@163.com>
2022-10-15 10:49:41 +0800

    暂时应该是没啥问题了

--- 潮流计算.py
+++ 潮流计算.py
@@ -11,22 +11,22 @@ import numpy as np
 
 """节点导纳矩阵"""
 # TODO: 可能要加一个生成节点导纳矩阵的函数，把那几个生成函数都放到其他文件里面
-Y = np.array([[2-4.j, -2.+4.j],
-              [-2+4.j, 2-4.j]])
+Y = np.array([[2 - 4.j, -2. + 4.j],
+              [-2 + 4.j, 2 - 4.j]])
 
 """给定PQ节点PV节点的已知量"""
-Ps = np.array([0.2])  # n-1
-Qs = np.array([0.1])  # m-1
+Ps = np.array([-0.2])  # n-1
+Qs = np.array([-0.1])  # m-1
 Us = np.array([])  # n-m
 
 """初始值"""
-e = np.array([0, 0])  # n
-f = np.array([1, 1])  # n
+e = np.array([1, 1])  # n
+f = np.array([0, 0])  # n
 
 G = np.real(Y)
 B = np.imag(Y)
 
-"""这个n是实际的n-1"""
+"""这个n是实际的节点数n-1"""
 n = len(Ps)
 m = len(Qs)
 
@@ -35,7 +35,8 @@ delta_P = np.zeros(n)
 delta_Q = np.zeros(m)
 delta_U2 = np.zeros(n - m)
 
-epsilon = 0.0001
+"""精度"""
+EPSILON = 0.0001
 
 
 def g_delta(nn, mm):
@@ -43,24 +44,27 @@ def g_delta(nn, mm):
     for i in range(nn):
         delta_P[i] = Ps[i] - sum(
             [e[i] * (G[i, j] * e[j] - B[i, j] * f[j]) + f[i] * (G[i, j] * f[j] + B[i, j] * e[j])
-             for j in range(nn)])
+             for j in range(nn + 1)])
 
     for i in range(mm):
+        # DONE 这里有问题
         delta_Q[i] = Qs[i] - sum(
-            [f[i] * (G[i, j] * e[j] - B[i, j] * f[j]) + e[i] * (G[i, j] * f[j] + B[i, j] * e[j])
-             for j in range(mm)])
+            [f[i] * (G[i, j] * e[j] - B[i, j] * f[j]) - e[i] * (G[i, j] * f[j] + B[i, j] * e[j])
+             for j in range(mm + 1)])
 
     for i in range(nn - mm):
         delta_U2[i] = Us[i] ** 2 - (e[i] ** 2 + f[i] ** 2)
+    try:
+        delta_PQU2 = np.vstack((delta_P.T, delta_Q.T, delta_U2.T))
+    except:
+        delta_PQU2 = np.vstack((delta_P.T, delta_Q.T))
+    # for i in range(mm):
+    #     delta_PQU2 = np.vstack((delta_PQU2, delta_P[i], delta_Q[i]))
+    #
+    # for i in range(mm, nn):
+    #     delta_PQU2 = np.vstack((delta_PQU2, delta_P[i], delta_U2[i]))
 
-    delta_PQU2 = np.empty(1)
-    for i in range(mm):
-        delta_PQU2 = np.vstack((delta_PQU2, delta_P[i], delta_Q[i]))
-
-    for i in range(mm, nn):
-        delta_PQU2 = np.vstack((delta_PQU2, delta_P[i], delta_U2[i]))
-
-    return np.delete(delta_PQU2, [0])
+    return delta_PQU2
 
 
 def jacobi(nn, mm):
@@ -72,14 +76,14 @@ def jacobi(nn, mm):
     R = np.zeros((nn - mm, nn))
     S = np.zeros((nn - mm, nn))
 
-    # TODO: 用一堆迭代分别求H, N, J, L, R, S
+    # DONE: 用一堆迭代分别求H, N, J, L, R, S
     for i in range(nn):
         for j in range(nn):
             if i != j:
                 H[i, j] = B[i, j] * e[i] - G[i, j] * f[i]
             else:
                 H[i, i] = B[i, i] * e[i] - G[i, i] * f[i] - \
-                          sum([G[i, jj] * f[jj] + B[i, jj] * e[jj] for jj in range(nn)])
+                          sum([G[i, jj] * f[jj] + B[i, jj] * e[jj] for jj in range(nn + 1)])
 
     for i in range(nn):
         for j in range(nn):
@@ -87,7 +91,7 @@ def jacobi(nn, mm):
                 N[i, j] = -G[i, j] * e[i] - B[i, j] * f[i]
             else:
                 N[i, i] = -G[i, j] * e[i] - B[i, i] * f[i] - \
-                          sum([G[i, jj] * e[jj] - B[i, j] * f[jj] for jj in range(nn)])
+                          sum([G[i, jj] * e[jj] - B[i, j] * f[jj] for jj in range(nn + 1)])
 
     for i in range(mm):
         for j in range(nn):
@@ -95,7 +99,7 @@ def jacobi(nn, mm):
                 J[i, j] = G[i, j] * e[i] + B[i, j] * f[i]
             else:
                 J[i, j] = G[i, j] * e[i] + B[i, j] * f[i] - \
-                          sum([G[i, jj] * e[jj] - B[i, j] * f[jj] for jj in range(nn)])
+                          sum([G[i, jj] * e[jj] - B[i, j] * f[jj] for jj in range(nn + 1)])
 
     for i in range(mm):
         for j in range(nn):
@@ -103,7 +107,7 @@ def jacobi(nn, mm):
                 L[i, j] = B[i, j] * e[i] - G[i, j] * f[i]
             else:
                 L[i, i] = B[i, j] * e[i] - G[i, j] * f[i] + \
-                          sum([G[i, jj] * f[jj] + B[i, jj] * e[jj] for jj in range(nn)])
+                          sum([G[i, jj] * f[jj] + B[i, jj] * e[jj] for jj in range(nn + 1)])
 
     for i in range(nn - mm):
         for j in (nn):
@@ -128,18 +132,33 @@ def jacobi(nn, mm):
 
 if __name__ == '__main__':
     error = np.array([])
+    i = 0
     while True:
         """main loop"""
-        i = 0
         delta_PQU2 = g_delta(n, m)
-        # TODO 雅克比矩阵这里有bug,求出来是奇异矩阵
+        # DONE 雅克比矩阵这里有bug,求出来是奇异矩阵
         J = jacobi(n, m)
         inv_J = np.linalg.inv(J)
         delta_fe = np.dot(inv_J, delta_PQU2)
-        f += delta_fe[::2]
-        e += delta_fe[1::2]
+        # _ = f[:n].copy()
+        # DONE 这里有一点问题，其他似乎是已经没问题了
+        # fe = np.vstack((f, e))
+        # fe[:, :n] = fe[:, :n] - delta_fe
+        z = np.zeros((2, 1))
+        delta_fe = np.c_[delta_fe, z]
+        f = f - delta_fe[0, :]
+        # _ = e[:n].copy()
+        e = e - delta_fe[1, :]
+        # e[:n] = _
+        # [f[:n], e[:n]] = [f[:n], e[:n]] - delta_fe
         # error.append(max(delta_fe))
-        np.append(error, max(delta_fe))
-        if error[i] <= epsilon:
+        error = np.append(error, delta_fe.max())
+        print(i)
+        print(error)
+        print(f, e)
+        if error[i] <= EPSILON:
             break
         i += 1
+
+
+
